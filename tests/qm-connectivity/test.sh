83fc1e633515b426391cb5e3bb9cde8dfb5aa572
Douglas Landgraf <dougsland@redhat.com>
2023-07-21 11:42:15 -0400

    tests: fix pre-commit warnings in bash/python (#125)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- tests/qm-connectivity/test.sh
+++ tests/qm-connectivity/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -eux
 
+# shellcheck disable=SC1094
 # shellcheck source=tests/e2e/lib/utils
 . ../e2e/lib/utils
 
-# shellcheck source=tests/e2e/lib/tests
+# shellcheck disable=SC1094
+# shellcheck source=tests/e2e/lib/utils
 . ../e2e/lib/tests
 
 test_hirte_list_all_units
