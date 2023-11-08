6cd683b7ad5fce5df929bb358d1f7eaa21b39b78
Yariv Rachmani <yrachman@redhat.com>
2023-11-08 22:24:46 +0200

    Fixing tier-1 tests
    Tmt connectivity test fo AutoSD container was broken
    
    Signed-off-by: Yariv Rachmani <yrachman@redhat.com>

--- tests/qm-connectivity/test.sh
+++ tests/qm-connectivity/test.sh
@@ -2,10 +2,10 @@
 
 # shellcheck disable=SC1094
 # shellcheck source=tests/e2e/lib/utils
-. ../e2e/lib/utils
+source ../e2e/lib/utils
 
 # shellcheck disable=SC1094
 # shellcheck source=tests/e2e/lib/utils
-. ../e2e/lib/tests
+source ../e2e/lib/tests
 
 test_bluechi_list_all_units
