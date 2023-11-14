f15be9488c309b31c1f4d43824375214085582d9
Yariv Rachmani <yrachman@redhat.com>
2023-11-14 15:32:54 +0200

    Add new test qm-unit-files
    This tests resolved #216
    
    Signed-off-by: Yariv Rachmani <yrachman@redhat.com>

---
+++ tests/qm-unit-files/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# shellcheck disable=SC1094
+# shellcheck source=tests/e2e/lib/utils
+source ../e2e/lib/utils
+
+CONTROL_CONTAINER_NAME="${CONTROL_CONTAINER_NAME:-control}"
+
+test_qm_readonly_unit_file() {
+
+    local qm_cmd
+
+    echo
+    info_message "Connected to \033[92m${CONTROL_CONTAINER_NAME}\033[0m, trying to update unit file"
+    if [  "${CONTROL_CONTAINER_NAME}" != "host" ]; then
+        qm_cmd="podman exec -c "
+        qm_cmd+="${CONTROL_CONTAINER_NAME} "
+    fi
+    qm_cmd+="'podman exec qm bash -c "echo true >> /usr/lib/systemd/system/bluechi-agent.service"'  2>&1"
+    sleep 1
+    cmd_result=$(eval "${qm_cmd}")
+
+    if_error_exit "unable to execute qm command on ${CONTROL_CONTAINER_NAME}"
+
+    grep -E  "Read-only file system" <<< "${cmd_result}"
+
+}
