9bdb2a59654e5bee1f9f2a566503ffa95be17063
Yariv Rachmani <yrachman@redhat.com>
2023-12-26 22:32:18 +0200

    Add bluechi-agent flood test
    Signed-off-by: Yariv Rachmani <yrachman@redhat.com>

--- tests/ffi/common/prepare.sh
+++ tests/ffi/common/prepare.sh
@@ -17,6 +17,8 @@ prepare_test() {
    if ! grep "Volatile" "${qm_service_file}" ; then
      exec_cmd "sed -i 's|ReadOnly=true|&\nVolatileTmp=true|' ${qm_service_file}"
    fi
+   # FIXME: QM is failing to start run podman #297 on asil space
+   exec_cmd "restorecon -RFv /var/lib/containers &> /tmp/asil-restorecon"
 }
 
 disk_cleanup() {
@@ -31,4 +33,33 @@ disk_cleanup() {
 reload_config() {
    exec_cmd "systemctl daemon-reload"
    exec_cmd "systemctl restart qm"
+}
+
+prepare_images() {
+   exec_cmd "podman pull quay.io/centos-sig-automotive/ffi-tools:latest"
+   # Copy container image registry to /var/qm/lib/containers
+   image_id=$(podman images | grep quay.io/centos-sig-automotive/ffi-tools | awk -F " " '{print $3}')
+   if [ ! -d "${QM_HOST_REGISTRY_DIR}" ]; then
+       exec_cmd "mkdir -p ${QM_HOST_REGISTRY_DIR}"
+       exec_cmd "podman push ${image_id} dir:${QM_HOST_REGISTRY_DIR}/tools-ffi:latest"
+       # Remove image to save /var space
+       exec_cmd "podman image rm ${image_id}"
+   fi
+}
+
+run_container_in_qm() {
+   local container_name
+   local tmp_image_dir
+   local run_ctr_in_qm
+   # Clean tmp image directory
+   tmp_image_dir=$(podman info | grep  CopyTmp | awk -F":" '{print $2}')
+   container_name="${1}"
+
+   exec_cmd "podman exec -it qm /bin/bash -c \
+      \"rm -rf ${tmp_image_dir}/*\""
+   run_ctr_in_qm="podman exec -it qm /bin/bash -c \
+      \"podman run -d --net host  --replace --name ${container_name} \
+      dir:${QM_REGISTRY_DIR}/tools-ffi:latest \
+      tail -f /dev/null\""
+   exec_cmd "${run_ctr_in_qm}"
 }
