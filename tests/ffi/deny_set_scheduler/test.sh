5d2c402181a14d2e867f7193d98784463876a67f
pengshanyu <yupengshan@hotmail.com>
2024-05-30 13:50:46 +0800

    add deny_set_scheduler
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>

---
+++ tests/ffi/deny_set_scheduler/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -euvx
+
+. ../common/prepare.sh
+
+disk_cleanup
+prepare_test
+reload_config
+
+podman exec -it qm /bin/bash -c \
+         'podman run -d  --replace --name ffi-qm \
+          quay.io/centos-sig-automotive/ffi-tools:latest \
+          tail -f /dev/null'
+
+return_from_setscheduler=$(podman exec -it qm /bin/bash -c \
+         'podman exec -it ffi-qm ./QM/test_sched_setscheduler')
+
+
+if [[ "${return_from_setscheduler}" =~ "Failed to set scheduler: Operation not permitted" ]]; then
+    info_message "set_scheduler() syscall denied in QM."
+else
+    info_message "set_scheduler() syscall can be executed in QM."
+    exit 1
+fi
