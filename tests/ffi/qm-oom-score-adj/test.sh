e26a131d785f84fb26a036e122961c9919892760
Roberto Majadas <rmajadas@redhat.com>
2024-06-20 10:22:43 +0200

    ffi: Add info messages to the qm-omm-score-adj test
    Signed-off-by: Roberto Majadas <rmajadas@redhat.com>

--- tests/ffi/qm-oom-score-adj/test.sh
+++ tests/ffi/qm-oom-score-adj/test.sh
@@ -31,11 +31,23 @@ QM_FFI_OOM_SCORE_ADJ=$(podman exec -it qm /bin/bash -c "cat /proc/$QM_FFI_PID/oo
 
 
 # "500" is the oom_score_adj defined for the qm/qm.container.
-test "$QM_OOM_SCORE_ADJ" -eq "500"
+if [ "$QM_OOM_SCORE_ADJ" -eq "500" ]; then
+    info_message "PASS: qm.container oom_score_adj value == 500"
+else
+    info_message "FAIL: qm.container oom_score_adj value != 500"
+    exit 1
+fi
+
 
 # "750" is the oom_score_adj defined in the qm/containers.conf as default value
 # for the containers that would run inside of the qm container.
-test "$QM_FFI_OOM_SCORE_ADJ" -eq "750"
+if [ "$QM_FFI_OOM_SCORE_ADJ" -eq "750" ]; then
+    info_message "PASS: qm containers oom_score_adj == 750"
+else
+    info_message "FAIL: qm containers oom_score_adj != 750"
+    exit 1
+fi
+
 
 podman exec -it qm /bin/bash -c "podman stop ffi-qm > /dev/null"
 
