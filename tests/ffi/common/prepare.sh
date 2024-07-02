ef8054176898049dc5ec432d7170e28f97e7beed
Roberto Majadas <rmajadas@redhat.com>
2024-07-02 11:35:38 +0200

    tests: Allow to tests to define their own qm container custom conf
    Signed-off-by: Roberto Majadas <rmajadas@redhat.com>

--- tests/ffi/common/prepare.sh
+++ tests/ffi/common/prepare.sh
@@ -12,6 +12,8 @@ prepare_test() {
    exec_cmd "sed -i 's|DropCapability=sys_resource|#DropCapability=sys_resource|' \
             ${qm_service_file}"
    exec_cmd "restorecon -RFv /var/lib/containers"
+   # Create qm container custom config folder
+   exec_cmd "mkdir -p /etc/containers/systemd/qm.container.d"
    # FIXME: This action should be performed if necessary through systemd drop-in files.
    #
    # # Changing QM score to 1000 to avoid full memory error on SoC
@@ -24,6 +26,8 @@ disk_cleanup() {
    exec_cmd "systemctl stop qm"
    remove_file=$(find /var/qm -size  +2G)
    exec_cmd "rm -f $remove_file"
+   remove_qm_custom_conf_files=$(find /etc/containers/systemd/qm.container.d -type f)
+   exec_cmd "rm -f $remove_qm_custom_conf_files"
    exec_cmd "systemctl start qm"
    remove_file=$(find /root -size  +1G)
    exec_cmd "rm -f $remove_file"
