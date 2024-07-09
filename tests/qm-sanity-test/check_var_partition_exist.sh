4dfad9c64db7ea1aa0359862a9ae9a8c05e34199
pengshanyu <yupengshan@hotmail.com>
2024-07-09 22:25:43 +0800

    add tier-0 test case
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    delete blank Lines
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    trim trailing whitespace
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    update function name and implementation
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    added:check /var on ostree
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    rename old tier-0 to multi-bluechi-agents;rename sanity test to tier-0
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    trailing-whitespace
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    remove old tier 0
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    split sanity test to 6 tests
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    trailing-whitespace
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    not number the tests;add main.fmf;manual_trigger multi-bluechi-agents
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    correct typo
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>
    
    add polarion id
    
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>

---
+++ tests/qm-sanity-test/check_var_partition_exist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -euvx
+
+# shellcheck disable=SC1091
+source ../e2e/lib/utils
+
+# Verify /var partition exist
+check_var_partition_exist(){
+    # /var on ostree image
+    if stat /run/ostree-booted > /dev/null 2>&1; then
+        expected_var_partition="part /var"
+    # /var on c9s image
+    else
+        expected_var_partition="part /usr/lib/qm/rootfs/var"
+    fi
+
+    if [[ "$(lsblk -o 'MAJ:MIN,TYPE,MOUNTPOINTS')" =~ ${expected_var_partition} ]]; then
+        info_message "check_var_partition_exist: /var exists in QM"
+        info_message "PASS: check_var_partition_exist()"
+        exit 0
+    else
+        info_message "FAIL: check_var_partition_exist: /var does not exists in QM"
+        exit 1
+    fi
+}
+
+check_var_partition_exist
