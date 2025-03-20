792b70bee9a767c8b97b993653594dafdaf7d72a
Michael Engel <mengel@redhat.com>
2025-03-20 09:59:41 +0100

    next
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- tests/qm-sanity-test/check_var_partition_exist.sh
+++ tests/qm-sanity-test/check_var_partition_exist.sh
@@ -5,6 +5,9 @@ source ../e2e/lib/utils
 
 # Verify /var partition exist
 check_var_partition_exist(){
+    info_message "Journal for qm bluechi-agent:\n"
+    journalctl -r -u qm -n 100
+
     # /var on ostree image
     if stat /run/ostree-booted > /dev/null 2>&1; then
         expected_var_partition="part /var"
@@ -21,9 +24,6 @@ check_var_partition_exist(){
         info_message "FAIL: check_var_partition_exist: /var does not exists in QM"
         exit 1
     fi
-
-    info_message "Journal for qm bluechi-agent:\n"
-    journalctl -r -u qm -n 100
 }
 
 check_var_partition_exist
