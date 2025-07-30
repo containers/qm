218a5877a726c2bcc86a159484db2dfdc35e826d
Douglas Schilling Landgraf <dougsland@redhat.com>
2025-07-30 10:08:45 -0400

    Revert "sched_setscheduler to allow other, batch idle (#824)"
    This reverts commit 10c87d916d9ced50a26c33802a2b1cf72bbf0f8c.

--- tests/ffi/deny_set_scheduler/test.sh
+++ tests/ffi/deny_set_scheduler/test.sh
@@ -3,7 +3,7 @@
 # shellcheck source=tests/ffi/common/prepare.sh
 . ../common/prepare.sh
 
-expected_result="Failed to set scheduler: Function not implemented"
+expected_result="Failed to set scheduler: Operation not permitted"
 
 trap disk_cleanup EXIT
 prepare_test
