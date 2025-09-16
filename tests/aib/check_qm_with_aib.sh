9e5fe83309c6531601ecf96c8afd451001769878
Yariv Rachmani <yrachman@redhat.com>
2025-09-16 16:06:50 +0300

    Fix broken aib test
    Signed-off-by: Yariv Rachmani <yrachman@redhat.com>

--- tests/aib/check_qm_with_aib.sh
+++ tests/aib/check_qm_with_aib.sh
@@ -9,10 +9,10 @@ exec_cmd "curl -o qm.aib.yml https://gitlab.com/CentOS/automotive/src/automotive
 USE_QM_COPR="${PACKIT_COPR_PROJECT:-@centos-automotive-sig/qm-next}"
 COPR_URL="https://download.copr.fedorainfracloud.org/results/${USE_QM_COPR}/epel-9-$(uname -m)/"
 #shellcheck disable=SC2089
-EXTRA_REPOS='extra_repos=[{"id":"qm_build","baseurl":"'"$COPR_URL"'"}]'
+EXTRA_REPOS="extra_repos=[{id: qm_build, baseurl: $COPR_URL}]"
 
 
 # Run AIB in container
 exec_cmd "curl -o auto-image-builder.sh https://gitlab.com/CentOS/automotive/src/automotive-image-builder/-/raw/main/auto-image-builder.sh?ref_type=heads"
 #shellcheck disable=SC2027,SC2090,SC2086
-exec_cmd "/bin/bash auto-image-builder.sh build --export qcow2 --define '"$EXTRA_REPOS"' qm.aib.yml qm.qcow2"
+exec_cmd "/bin/bash auto-image-builder.sh build --export qcow2 --define \\\"$EXTRA_REPOS\\\" qm.aib.yml qm.qcow2"
