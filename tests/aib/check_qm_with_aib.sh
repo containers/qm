32579f981660c6815c5c609240754ac23ea025d1
Douglas Schilling Landgraf <dougsland@redhat.com>
2025-12-19 10:31:02 -0500

    aib: update params to sync with upstream
    Avoid tests breakage.
    
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- tests/aib/check_qm_with_aib.sh
+++ tests/aib/check_qm_with_aib.sh
@@ -15,4 +15,5 @@ EXTRA_REPOS="extra_repos=[{id: qm_build, baseurl: $COPR_URL}]"
 # Run AIB in container
 exec_cmd "curl -o auto-image-builder.sh https://gitlab.com/CentOS/automotive/src/automotive-image-builder/-/raw/main/auto-image-builder.sh?ref_type=heads"
 #shellcheck disable=SC2027,SC2090,SC2086
-exec_cmd "/bin/bash auto-image-builder.sh build --export qcow2 --define \\\"$EXTRA_REPOS\\\" qm.aib.yml qm.qcow2"
+#/bin/bash auto-image-builder.sh build --target qemu --define "$EXTRA_REPOS" qm.aib.yml qm.qcow2 || if_error_exit "AIB build failed"
+exec_cmd "/bin/bash auto-image-builder.sh build --target qemu --define \\\"$EXTRA_REPOS\\\" qm.aib.yml qm.qcow2"
