1a49fa1990dbaae19a08a9d60de1bedf9e023694
Douglas Schilling Landgraf <dougsland@redhat.com>
2026-01-21 10:29:01 -0500

    ci/cd: fix libkrun issue Internal(Vm)
    fixes Internal(Vm(SetMemoryAttributes(Error(22)))) issue.
    libkrun 1.17 includes the issue but currently it's in rawhide only.
    
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- tests/qm-kvm-test/libkrun/check_libkrun.sh
+++ tests/qm-kvm-test/libkrun/check_libkrun.sh
@@ -11,7 +11,15 @@ enable_repo() {
 
 install_libkrun() {
     info_message "install_libkrun(): install libkrun and crun-krun"
-    exec_cmd "dnf install --setopt=reposdir=/etc/yum.repos.d  --installroot=/usr/lib/qm/rootfs -y libkrun crun-krun"
+    # Adding rawhide as 1.17 fixes Internal(Vm(SetMemoryAttributes(Error(22))))
+    # See-Also: https://github.com/containers/qm/pull/959
+    if grep -qi "^ID=fedora" /etc/os-release; then
+        # Need the remove as soon the land in stable channel
+        exec_cmd "dnf install --installroot=/usr/lib/qm/rootfs -y fedora-repos-rawhide"
+    fi
+    exec_cmd "cat /usr/lib/qm/rootfs/etc/yum.repos.d/*"
+    exec_cmd "dnf repolist --all --setopt=reposdir=/etc/yum.repos.d"
+    exec_cmd "dnf install --use-host-config --releasever=rawhide --enablerepo=rawhide --setopt=reposdir=/etc/yum.repos.d  --installroot=/usr/lib/qm/rootfs -y libkrun crun-krun libkrunfw"
 }
 
 check_libkrun() {
@@ -19,6 +27,10 @@ check_libkrun() {
     exec_cmd "podman exec -it qm podman run --runtime=krun --rm -it alpine echo 'Hello libkrun.'"
     info_message "PASS: libkrun runs successfully."
 }
+
+#if grep -qi "^ID=centos" /etc/os-release; then
+#    enable_repo
+#fi
 
 enable_repo
 install_libkrun
