ce491bcba98b6b092a9a22c41e4d0665d0b11671
pengshanyu <yupengshan@hotmail.com>
2026-01-22 18:31:51 +0800

    debug libkrun on f42
    Signed-off-by: pengshanyu <yupengshan@hotmail.com>

--- tests/qm-kvm-test/libkrun/check_libkrun.sh
+++ tests/qm-kvm-test/libkrun/check_libkrun.sh
@@ -20,6 +20,19 @@ check_libkrun() {
     info_message "PASS: libkrun runs successfully."
 }
 
+verify_kvm_exposed_to_qm() {
+    info_message "verify_kvm_exposed_to_qm(): check if /dev/kvm is exposed to qm."
+
+    # Check /dev/kvm in qm
+    if podman exec qm test -c /dev/kvm; then
+        info_message "PASS: /dev/kvm is exposed to qm."
+    else
+        info_message "FAIL: /dev/kvm is not exposed to qm."
+    fi
+}
+
+test -f /usr/share/qm/seccomp-no-rt.json && grep -i "memfd" /usr/share/qm/seccomp-no-rt.json || echo "File not found or memfd not in profile"
+verify_kvm_exposed_to_qm
 enable_repo
 install_libkrun
 check_libkrun
